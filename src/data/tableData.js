99453d9fea4aae73761b48205cb49e45a8657e27
Igor Mokhov <igormokhovid@gmail.com>
2024-12-20 12:33:27 +0300

    feat: added tableData

---
+++ src/data/tableData.js
@@ -0,0 +1,57 @@
+export const tableData = [
+  {
+    label: 'Наличные',
+    currentDay: 300000,
+    yesterday: 150000,
+    thisWeek: 200000,
+  },
+  {
+    label: 'Безналичный расчет',
+    currentDay: 287000,
+    yesterday: 100000,
+    thisWeek: 156000,
+  },
+  {
+    label: 'Кредитные карты',
+    currentDay: 100521,
+    yesterday: 200521,
+    thisWeek: 300521,
+  },
+  {
+    label: 'Средний чек, руб',
+    currentDay: 1300,
+    yesterday: 900,
+    thisWeek: 900,
+  },
+  {
+    label: 'Средний гость, руб',
+    currentDay: 200,
+    yesterday: 1200,
+    thisWeek: 600,
+  },
+  {
+    label: 'Удаления из чека (после оплаты), руб',
+    currentDay: 100,
+    yesterday: 200,
+    thisWeek: 900,
+  },
+  {
+    label: 'Удаления из чека (до оплаты), руб',
+    currentDay: 1300,
+    yesterday: 1300,
+    thisWeek: 900,
+  },
+  {
+    label: 'Количество чеков',
+    currentDay: 34,
+    yesterday: 36,
+    thisWeek: 34,
+  },
+
+  {
+    label: 'Количество гостей',
+    currentDay: 34,
+    yesterday: 36,
+    thisWeek: 32,
+  },
+];
